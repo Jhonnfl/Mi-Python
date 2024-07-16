bd161e10aa9fe846155489cb5facf83ae87554ed
jhonnfl <inversionesjhonn@gmail.com>
2024-07-16 12:53:52 -0400

    Ejercicio de almacenar datos personales en variables

--- variables.py
+++ variables.py
@@ -14,3 +14,15 @@ print(z)
 #Como ejemplo queremos ver la posicion de memoria de la variable "x"
 #Hay que tener en cuenta que la posicion de una variable cambia asi que la posicion no es fija
 print(id(x))
+print("")
+
+#Ejercicio de variable donde se almacene un Nombre, Numero de telefono y un email (Esto se debe mostrar en cosola)
+
+
+nombre = "Jhonn Flores"
+telefono = "+58 424-8636952"
+email = "jhonnhh@gmail.com"
+
+print(nombre)
+print(telefono)
+print(email)
