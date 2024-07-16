950befe5da523990323665504b21ba3ac3f832cc
jhonnfl <inversionesjhonn@gmail.com>
2024-07-16 14:49:13 -0400

    Tipos de datos Python

---
+++ tipos_datos.py
@@ -0,0 +1,25 @@
+#Para saber el tipo de dato de una variable debemos utilizar la funcion type()
+
+entero = 20
+flotante = 3.14
+texto = "Hola mundo"
+booleano = True #En cualquier caso puedes probar con "False" respetando las mayúsculas
+print(type(entero))
+print(type(flotante))
+print(type(texto))
+print(type(booleano))
+
+# Tipo de dato Numerico (Numeric)
+"""Podemos almacenar multiples valores como: Numeros enteros (Integer), Numeros Flotantes (Float), Numeros Complejos (Complex Number)"""
+
+# Tipo de dato Diccionario (Dictionary)
+
+
+# Tipo de dato Booleano 0 y 1, True y False (Boolean)
+
+
+# Tipo de dato Set
+
+
+# Tipo de dato Secuencia (Sequence)
+"""En tipos de datos de secuencia se pueden almacenar multiples valores como: Strings (Cadenas de texto), Listas (List) y Tuplas (Tuple) """
