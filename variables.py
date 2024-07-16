a6642797178c4018abc5f1c4e2af00f332d3c752
jhonnfl <inversionesjhonn@gmail.com>
2024-07-16 02:46:03 -0400

    Conceptos y ejemplos de variables

--- variables.py
+++ variables.py
@@ -1,3 +1,15 @@
+#Una variable es un dato que se almacena en la memoria
 miVariable = "Hola Mundo"
 
+#La funcion de Imprimir en consola es print()
 print(miVariable)
+
+#Ejemplo de variables:
+x = 10
+y = 2
+z = x + y
+print(z)
+
+#Para encontrar la posicion de memoria donde se guarda una variable puedes usar la funcion id()
+#Como ejemplo queremos ver la posicion de memoria de la variable "x"
+print(id(x))
